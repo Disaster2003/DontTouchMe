037505b32952be67fcda06c0298dae0d15430145
Disaster <Disaster20031017@gmail.com>
2024-09-04 16:57:33 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// シーンの状態
+    /// </summary>
+    enum SCENE_STATE
+    {
+        TITLE = 0,   // タイトル
+        EXPLAIN = 1, // 説明
+        PLAY = 2,    // プレイ
+        RESULT,      // 結果
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// クリックしたら、シーン遷移
+    /// </summary>
+    public void OnClickNextScene()
+    {
+        int buildIndex = SceneManager.GetActiveScene().buildIndex;
+        if(buildIndex == (int)SCENE_STATE.RESULT)
+            SceneManager.LoadSceneAsync(0);
+        else
+            SceneManager.LoadSceneAsync(buildIndex + 1);
+    }
+}
